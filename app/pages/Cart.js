f5b76f1d8b6058828c7677cc8607898e0e7f1c24
Mattia Mari <mattiamari96@gmail.com>
2020-02-03 22:02:51 +0100

    Partial. Cart page

--- app/pages/Cart.js
+++ app/pages/Cart.js
@@ -1,53 +1,78 @@
 'use strict';
 
 import Cart from '../model/Cart.js';
+import Language from '../model/Language.js';
 import NavBar from '../components/NavBar.js';
+import htmlToElement from '../utils/htmlToElement.js';
 
 const PriceRow = priceDetail => {
     return /*html*/`
         <li>
             <span class="ticket-type" aria-label="Ticket type">${priceDetail.name}</span>
-            <span class="ticket-price" aria-label="Ticket price">${priceDetail.price}</span>
+            <span class="ticket-price" aria-label="Ticket price">${Language.formatCurrency(priceDetail.price)}</span>
         </li>
     `;
 };
 
-const Card = ticket => {
-    const priceList = ticket.prices.map(e => PriceRow(e)).join('\n');
+class Card {
+    constructor(ticket) {
+        this.ticket = ticket;
+        this.card = null;
+    }
 
-    return /*html*/`
-        <li>
-            <div>
-                <span class="event-name" aria-label="Event name">${ticket.event.title}</span>
-                <span class="event-location" aria-label="Event location">${ticket.event.location}</span>
-                <span class="event-date" aria-label="Event date">${ticket.event.date}</span>
+    render() {
+        const priceList = this.ticket.prices.map(e => PriceRow(e)).join('\n');
+        
+        const template = /*html*/`
+            <li>
+                <div>
+                    <span class="event-name" aria-label="Event name">${this.ticket.event.title}</span>
+                    <span class="event-location" aria-label="Event location">${this.ticket.event.location}</span>
+                    <span class="event-date" aria-label="Event date">${this.ticket.event.date}</span>
 
-                <ul class="priceList">
-                    ${priceList}
+                    <ul class="priceList">
+                        ${priceList}
 
-                    <li class="ticket-totalPrice">
-                        <span>Totale</span>
-                        <span class="ticket-price" aria-label="Ticket price">${ticket.totalPrice}</span>
-                    </li>
-                </ul>                
-            </div>
+                        <li class="ticket-totalPrice">
+                            <span>Totale</span>
+                            <span class="ticket-price" aria-label="Ticket price">${Language.formatCurrency(this.ticket.totalPrice)}</span>
+                        </li>
+                    </ul>                
+                </div>
 
-            <button class="button button--flat btnRemove" aria-label="Remove from cart">Rimuovi</button>
-        </li>
-    `;
-};
+                <button class="button button--flat btnRemove" aria-label="Remove from cart">Rimuovi</button>
+            </li>
+        `;
+
+        this.card = htmlToElement(template);
+
+        this.card.querySelector('button.btnRemove').onclick = () => {
+            Cart.removeTicket(this.ticket);
+            this.animateRemove();
+        };
+
+        return this.card;
+    }
+
+    animateRemove() {
+        this.card.classList.add('animate--removeListItem');
+
+        window.setTimeout(() => {
+            this.card.remove();
+        }, 2000);
+    }
+}
 
 class CartPage {
     constructor(params) {
+        this.page = null;
     }
 
     async render() {
         const navbar = await NavBar.render();
         await Cart.init();
-        const tickets = Cart.tickets;
-        const ticketCards = tickets.map(e => Card(e)).join('\n');
 
-        return /*html*/`
+        const template = /*html*/`
             <div class="page page--cart">
                 ${navbar}
 
@@ -58,19 +83,32 @@ class CartPage {
                 </header>
 
                 <main>
-                    <ul class="cartList">
-                        ${ticketCards}
+                    <ul class="cartList">  
                     </ul>
 
                     <a class="button button--outline btnNext" href="#/purchase-complete">Completa l'ordine</a>
                 </main>
             </div>
         `;
+
+        this.page = htmlToElement(template);
+        this.updateList();
+
+        return this.page;
+    }
+
+    async updateList() {
+        const ticketCards = Cart.tickets.map(e => new Card(e).render());
+        const cartList = this.page.querySelector('.cartList');
+        cartList.innerHTML = '';
+        ticketCards.forEach(e => cartList.append(e));
     }
 
     afterRender() {
         NavBar.afterRender(document.getElementById('navbar'));
     }
+
+
 };
 
 export default CartPage;
