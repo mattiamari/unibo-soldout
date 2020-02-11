fcdc12de5b26926ed4e8982d7fea3c644bab6205
Mattia Mari <mattiamari96@gmail.com>
2020-02-11 10:57:27 +0100

    Tabbed container

--- app/components/TabbedContainer.js
+++ app/components/TabbedContainer.js
@@ -1,27 +1,44 @@
 'use strict';
 
-import htmlToElement from "../utils/htmlToElement.js";
+import htmlToElement from '../utils/htmlToElement.js';
+import throttle from '../utils/throttle.js';
 
-const bindings = new WeakMap();
-
-const Slide = content => {
-    const element = document.createElement('section');
-    element.classList.add('tabbedContainer-slide');
-
-    if (content instanceof Array) {
-        element.append(...content);
-    } else {
-        element.append(content);
-    }
-
-    return element;
-};
-
-class TabButton {
-    constructor(name, slide, container) {
+class Slide {
+    constructor(name, content, container) {
         this.element = null;
         this.name = name;
-        this.slide = slide;
+        this.content = content;
+        this.container = container;
+    }
+
+    render() {
+        this.element = document.createElement('section');
+        this.element.classList.add('tabbedContainer-slide');
+
+        if (this.content instanceof Array) {
+            this.element.append(...this.content);
+        } else {
+            this.element.append(this.content);
+        }
+
+        this.container.element.addEventListener('tabChanged', () => {
+            if (this.container.activeTab == this.name) {
+                this.element.parentElement.scrollTo(this.element.offsetLeft, 0);
+            }
+        });
+    
+        return this.element;
+    }
+
+    get midpointOffset() {
+        return this.element.offsetLeft + (this.element.clientWidth / 2);
+    }
+}
+
+class TabButton {
+    constructor(name, container) {
+        this.element = null;
+        this.name = name;
         this.container = container;
     }
 
@@ -30,24 +47,29 @@ class TabButton {
         this.element.classList.add('button', 'button--tab');
         this.element.innerHTML = this.name;
         this.element.onclick = () => {
-            this.select();
+            this.container.selectTab(this.name);
         };
 
-        bindings.set(this.element, this);
+        this.container.element.addEventListener('tabScrolled', () => this.onTabChanged());
+
         return this.element;
     }
 
-    select() {
-        this.element.parentElement.querySelectorAll('.button--tab').forEach(e => {
-            bindings.get(e).deselect();
-        });
+    onTabChanged() {
+        if (this.container.activeTab != this.name) {
+            this.element.classList.remove('button--active');
+            return;
+        }
 
         this.element.classList.add('button--active');
-        this.container.scrollTo(this.slide.offsetLeft, 0);
-    }
 
-    deselect() {
-        this.element.classList.remove('button--active');
+        // Scroll to the selected button if it's not visible
+        if (this.element.offsetLeft + this.element.clientWidth > this.element.parentElement.scrollLeft + this.element.parentElement.clientWidth
+            || this.element.offsetLeft < this.element.parentElement.scrollLeft
+        ) {
+            this.element.parentElement.scrollTo(this.element.offsetLeft, 0);
+        }
+        
     }
 }
 
@@ -55,6 +77,7 @@ class TabbedContainer {
     constructor(content) {
         this.content = content;
         this.element = null;
+        this.activeTab = null;
     }
 
     render() {
@@ -69,14 +92,41 @@ class TabbedContainer {
 
         const nav = this.element.querySelector('nav');
         const slides = this.element.querySelector('.tabbedContainer-slides');
+        let slideObjs = [];
 
         for (let tab of this.content) {
-            const slide = Slide(tab.content);
-            nav.append((new TabButton(tab.name, slide, slides)).render());
-            slides.append(slide);
+            const button = new TabButton(tab.name, this);
+            const slide = new Slide(tab.name, tab.content, this);
+            slideObjs.push(slide);
+
+            nav.append(button.render());
+            slides.append(slide.render());
         }
 
+        slideObjs = slideObjs.sort((a,b) => a.element.offsetLeft - b.element.offsetLeft);
+
+        slides.addEventListener('scroll', throttle(200, () => {
+            const currentSlide = slideObjs.find(e => e.midpointOffset >= slides.scrollLeft
+                && e.midpointOffset < slides.scrollLeft + slides.clientWidth);
+            
+            if (currentSlide == this.activeTab) {
+                return;
+            }
+
+            this.activeTab = currentSlide.name;
+            this.element.dispatchEvent(new Event('tabScrolled'));
+        }));
+
+        // Select first tab on load
+        this.selectTab(this.content[0].name);
+        this.element.dispatchEvent(new Event('tabScrolled'));
+
         return this.element;
+    }
+
+    selectTab(name) {
+        this.activeTab = name;
+        this.element.dispatchEvent(new Event('tabChanged'));
     }
 }
 
