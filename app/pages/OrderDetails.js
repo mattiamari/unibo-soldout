0ba2c551d051cc8a5a323eb5db212aff87ce8746
Mattia Mari <mattiamari96@gmail.com>
2020-02-13 23:11:11 +0100

    Partial. Order details page

---
+++ app/pages/OrderDetails.js
@@ -0,0 +1,21 @@
+'use strict';
+
+import htmlToElement from "../utils/htmlToElement.js";
+
+class OrderDetailsPage {
+    constructor() {
+
+    }
+
+    render() {
+        const template = /*html*/ `
+
+        `;
+        
+        const page = htmlToElement(template);
+
+        return page;
+    }
+}
+
+export default OrderDetailsPage;
