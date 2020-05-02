4b18a7ba1aed23b86795174021dfe9c25deb53b8
Mattia Mari <mattiamari96@gmail.com>
2020-05-03 00:12:33 +0200

    Use local css instead of bulma from cdn

--- organizzatore/index.php
+++ organizzatore/index.php
@@ -10,6 +10,7 @@ session_start();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>soldOUT - Pannello organizzatore</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <?php if ($_SESSION['login']): ?>
