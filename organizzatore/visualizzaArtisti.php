ff09ccb3b4df097792fcddb82e677ddf7db5c202
Daniele Manfredonia <d.manfre10@gmail.com>
2020-06-05 23:25:51 +0200

    more work

--- organizzatore/visualizzaArtisti.php
+++ organizzatore/visualizzaArtisti.php
@@ -20,16 +20,21 @@ if (isset($_GET["id"]) && isset($_GET["action"])) {
 
 function artistRow($artist) {
   $status = "";
+  $statusElimina = "";
   $stringa = "Modifica";
-  if($artist['show_count'] > 0 && $_SESSION["is_admin"]==0) {
-    $status = "disabled";
-    $stringa = "Visualizza";
+  if($artist['show_count'] > 0) {
+    $statusElimina = "disabled";
+    if ($_SESSION["is_admin"]==0) {
+        $stringa = "Visualizza";
+        $status = "";
+    }
   }
+  //href=visualizzaArtisti.php?id={$artist['id']}&action=deleteArtist
     return "<tr>"
         . "<td headers='nome'>{$artist['name']}</td>"
         . "<td headers='descrizione'>{$artist['description']}</td>"
         . "<td headers='azione'><a class=\"button \" href=creazioneArtista.php?id={$artist['id']}>$stringa</td>"
-        . "<td headers='azione'><a class=\"button is-danger is-light is-outlined {$status} \"  {$status} href=visualizzaArtisti.php?id={$artist['id']}&action=deleteArtist>Elimina</td>"
+        . "<td headers='azione'><a data-artistid='{$artist['id']}' class=\"button elimina is-danger is-light is-outlined {$statusElimina} \"  {$statusElimina}>Elimina</td>"
         . "</tr>";
 }
 $artist = $db->countArtistWithShow();
@@ -49,11 +54,33 @@ $artist = $db->countArtistWithShow();
     <script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
     <script src="http://code.jquery.com/jquery-1.6.4.min.js"></script>
     <script src="./navbar.js"></script>
+    <script src="./eliminaArtista.js"></script>
     <script src="./disabilitaBottone.js"></script>
     
 </head>
 
 <body>
+
+    <div class="modal">
+        <div class="modal-background">
+
+        </div>
+        <div class="modal-card">
+            <header class="modal-card-head">
+                <p class="modal-card-title">Conferma</p>
+                <button class="delete" aria-label="close"></button>
+            </header>
+            <section class="modal-card-body">
+    <!-- Content ... -->
+                <p>Sicuro di voler eliminare questo artista?</p>
+            </section>
+            <footer class="modal-card-foot">
+                <a class="button conferma is-success">Conferma</a>
+                <button class="button annulla">Annulla</button>
+            </footer>
+        </div>
+    </div>
+
     <?php  require "navbarArtista.php"?>
     <section class="section container">
         <a id="newElementButton" href="creazioneArtista.php" class="button is-primary">Aggiungi artista</a>
